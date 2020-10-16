c8ba1cd5eedab2a44b367d4ab314c638241a8e80
makolvik <makolvik@ukr.net>
2020-10-16 22:20:37 +0300

    make it better

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const favicon = require('express-favicon');
+// const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8000;
 const app = express();
-// app.use(favicon(__dirname+ '/build' ));
+// app.use(favicon(__dirname + '/build' ));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname,'build')));
 app.get('/ping',function(req, res){
